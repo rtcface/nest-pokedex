3d77344446392521ceea61e5a310154275e17204
Ramiro Tepehua <rtcface@gmail.com>
2022-12-05 17:01:41 -0600

    add seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CommonModule } from './common/common.module';
     }),
     MongooseModule.forRoot('mongodb://pokemon:123456@localhost:27017/nest-pokedex'),
     CommonModule,
+    SeedModule,
   ], 
 })
 export class AppModule {}
